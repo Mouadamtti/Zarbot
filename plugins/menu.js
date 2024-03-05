675fff191be6769a3f12924cc220f9043090d6cd
Mouadamtti <158284959+Mouadamtti@users.noreply.github.com>
2024-03-05 21:18:53 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -138,7 +138,9 @@ contextInfo: {
 externalAdReply: {
 title: 'ZAR BOT ♥',
 body: "أول بوت واتساب في العالم العربي 💖",
-iaType: 1,
+thumbnailUrl: 'https://telegra.ph/file/2829c7653514416d207e2.jpg',
+sourceUrl: 'https://instagram.com/noureddine_ouafy',
+mediaType: 1,
 renderLargerThumbnail: true
 }}}, { quoted: m})
 
